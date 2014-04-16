19fcb26c49fa846a73f5ccc42bb63c52d06a5ea5
kentothemes <kentothemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-16 06:09:39 +0000

    Update 1.3
    git-svn-id: https://plugins.svn.wordpress.org/kento-pricing-table-free/tags/1.3@894089 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/kpt-ajax.js
+++ js/kpt-ajax.js
@@ -1,1 +1,23 @@
-jQuery(document).ready(function(e){e(".kpt-corner-gradient").change(function(){var t=e(this).val();e("#kpt-corner-gradient-value").html(t)});e("#kpt-total-column, #kpt-total-row").blur(function(){var t=e("#kpt-total-row").val();var n=e("#kpt-total-column").val();e.ajax({type:"POST",url:kpt_ajax.kpt_ajaxurl,data:{action:"kpt_ajax_form",kpt_total_row:t,kpt_total_column:n},success:function(t){e("#kpt-total-data").html(t)}})});e(".kpt-bg-color").wpColorPicker()})
+jQuery(document).ready(function(jQuery)
+	{
+	jQuery(".kpt-corner-gradient").change(function()
+		{
+			var kpt_corner_gradient_value=jQuery(this).val();
+			jQuery("#kpt-corner-gradient-value").html(kpt_corner_gradient_value)});
+		
+			jQuery("#kpt-total-column, #kpt-total-row").blur(function(){
+				var kpt_total_row=jQuery("#kpt-total-row").val();
+				var kpt_total_column=jQuery("#kpt-total-column").val();
+				jQuery.ajax(
+					{
+					type:"POST",
+					url:kpt_ajax.kpt_ajaxurl,
+					data:{action:"kpt_ajax_form",kpt_total_row:kpt_total_row,kpt_total_column:kpt_total_column},
+					success:function(data)
+						{
+						jQuery("#kpt-total-data").html(data)
+						}
+					})
+		});
+					
+	})
