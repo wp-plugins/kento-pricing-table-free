b21f672cac6c6d83bb392635a68073627d44197f
kentothemes <kentothemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-02 04:06:52 +0000

    1.3 update
    git-svn-id: https://plugins.svn.wordpress.org/kento-pricing-table-free/tags/1.3@924926 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- kpt-pro.php
+++ kpt-pro.php
@@ -18,22 +18,27 @@ If you need more feature for your pricing table then you could try our pro versi
 <br />
 <b>Pro Version Features</b><br />
 
-# Life Time Update.<br />
+# Life Time Auto Update.<br />
 # Life Time Support via forum.<br />
 # One Time purchase.<br />
 # 7 Days Refund.<br />
 # Easy to use.<br />
-# Four different themes.<br />
-# Custom font size for column header, price, signup link.<br />
-# Custom color for column header, price, signup link.<br />
-# Custom height for column header, price, signup.<br />
+# Different Background Color for column.<strong style="color:#139b50;">(Premium Only)</strong><br />
+# Four different themes.<strong style="color:#139b50;">(Premium Only)</strong><br />
+# Top And Bottom Gradient. <strong style="color:#139b50;">(Premium Only)</strong><br />
+# Column Corner Radius.<strong style="color:#139b50;">(Premium Only)</strong><br />
+# Table Column Margin.<strong style="color:#139b50;">(Premium Only)</strong><br />
+# Hide Blank Field Style. <strong style="color:#139b50;">(Premium Only)</strong><br />
+# Custom font size for column header, price, signup link.<strong style="color:#139b50;">(Premium Only)</strong><br />
+# Custom color for column header, price, signup link.<strong style="color:#139b50;">(Premium Only)</strong><br />
+# Custom height for column header, price, signup.<strong style="color:#139b50;">(Premium Only)</strong><br />
 # Unlimited Number Pricing Table Using shortcodes.<br />
 # Unlimited column color for each column.<br />
 # Featured Colum And Hover effect.<br />
 <br /><br />
 <h3>Pro Vesrion:</h3><br />
 <strong>Price: $5(USD)</strong><br />
-Link: <a target="_blank" href="http://kentothemes.com/items/plugins/wordpress-pricing-table/">http://kentothemes.com/items/plugins/wordpress-pricing-table/</a><br /><br />
+Link: <a target="_blank" href="http://kentothemes.com/items/plugins/kento-pricing-table/">http://kentothemes.com/items/plugins/kento-pricing-table/</a><br /><br />
 <img src="<?php echo  plugins_url(); ?>/kento-pricing-table-free/css/kento-pricing-table-pro.png" />
 </p>
 </div>
